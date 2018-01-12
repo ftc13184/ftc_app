e649a515eeb8e54464abc6af995e6a5e060e4b62
ftc13184-ku <radiating.robots@gmail.com>
2018-01-12 08:42:14 -0800

    changes to mirror the latest Red program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueParkSafeZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueParkSafeZone.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import static android.R.attr.left;
+import static android.R.attr.right;
+import static com.sun.tools.javac.main.Option.D;
+
 // below is the Annotation that registers this OpMode with the FtcRobotController app.
 // @Autonomous classifies the OpMode as autonomous, name is the OpMode title and the
 // optional group places the OpMode into the Exercises group.
@@ -19,6 +23,8 @@ public class BlueParkSafeZone extends LinearOpMode {
     Servo leftClaw;
     Servo rightClaw;
     DcMotor leftArm;
+    Servo sideArm;
+
     @Override
     public void runOpMode() throws InterruptedException
     {
@@ -28,12 +34,16 @@ public class BlueParkSafeZone extends LinearOpMode {
         leftClaw = hardwareMap.servo.get("left_hand");
         rightClaw = hardwareMap.servo.get("right_hand");
         leftArm = hardwareMap.dcMotor.get("left_arm");
+        sideArm = hardwareMap.servo.get("sensor_hand");
 
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
         // wait for start button.
         waitForStart();
+
+        //set side arm to starting position
+        sideArm.setPosition(0.2);
 
         telemetry.addData("Mode", "running");
         telemetry.update();
@@ -43,27 +53,55 @@ public class BlueParkSafeZone extends LinearOpMode {
         rightClaw.setPosition(-0.1);
 
         //pick the glyph of the ground
-        leftArm.setPower(0.15);
+        leftArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftArm.setPower(0.25);
+        sleep(2500);
+        leftArm.setPower(0);
 
         // set power to both motors to drive off balance board.
         leftMotor.setPower(0.30);
         rightMotor.setPower(0.30);
 
         // continue till we reach safe zone.
-        sleep(1200);
+        sleep(1600);
 
         // turn toward crypto box
-        leftMotor.setPower(0.0);
-        rightMotor.setPower(0.85);
+        leftMotor.setPower(-0.6);
+        rightMotor.setPower(0.6);
 
         //let turn for sufficient time to face box
-        sleep(3500);
+        sleep(1300);
 
-        //drive toward crypto box to get into safe zone
-        leftMotor.setPower(0.20);
-        rightMotor.setPower(0.20);
+        //move backward
+        leftMotor.setPower(-0.6);
+        rightMotor.setPower(-0.6);
 
-        sleep(500);
+        sleep(400);
+
+        //stop
+        leftMotor.setPower(0.0);
+        rightMotor.setPower(0.0);
+
+        //move center arm down
+        leftArm.setPower(-0.25);
+        sleep(2000);
+
+        //stop
+        leftArm.setPower(0);
+
+        //move forward
+        leftMotor.setPower(0.60);
+        rightMotor.setPower(0.60);
+        sleep(800);
+
+        //drop the glyph
+        leftClaw.setPosition(-1);
+        rightClaw.setPosition(1);
+
+        //move back
+        leftMotor.setPower(-0.25);
+        rightMotor.setPower(-0.25);
+        sleep(200);
 
         //stop
         leftMotor.setPower(0.0);
